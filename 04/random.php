94a406293266233abf7cdd02028081b3bf4e01d8
manuel medina <manuel.mv1191@gmail.com>
2015-12-14 09:39:40 -0500

    primer commit en php-for-the-web 14/12/15 9:40am

---
+++ 04/random.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>Lucky Numbers</title>
+</head>
+<body>
+	<?php // Script 4.6 - random.php
+	/* This script generates 3 random numbers. */
+
+	// Address error handling, if you want.
+
+	// Create three random numbers:
+	$n1 = rand (1, 99);
+	$n2 = rand (1, 99);
+	$n3 = rand (1, 99);
+	$maximo_rand= getrandmax();
+
+	// Print out the numbers:
+	print "<p>Your lucky numbers are:<br />
+	$n1<br />
+	$n2<br />
+	$n3<br />
+	El maximo valor random es: $maximo_rand</p>";
+
+	$n1=mt_rand();
+	$n2=mt_rand();
+	$n3=mt_rand();
+	print "<p> Using mt_rand, your lucky numbers are: <br />
+	$n1<br />
+	$n2<br />
+	$n3</p>"
+	?>
+</body>
+</html>
