c1b3ea126a65102b1bec1e74b9a2085b924c6f58
manuelMedina <manuel.mv1191@gmail.com>
2016-01-02 13:18:58 -0500

    ultimo commit en php-for-the-web 2/1/16 1:19pm

--- 11/view_quote.php
+++ 11/view_quote.php
@@ -10,18 +10,17 @@
 	<?php // Script 11.3 - view_quote.php
 	/* This script displays and handles an HTML form. This script reads in a file and prints a random line from it. */
 
-	// Read the file's contents into an array:
-	$data = file('../quotes.txt');
+		// Read the file's contents into an array:
+		$data = file('../quotes.txt');
 
-	// Count the number of items in the array:
-	$n = count($data);
+		// Count the number of items in the array:
+		$n = count($data);
 
-	// Pick a random item:
-	$rand = rand(0, ($n - 1));
+		// Pick a random item:
+		$rand = rand(0, ($n - 1));
 
-	// Print the quotation:
-	print '<p>' . trim($data[$rand]) . '</p>';
-
+		// Print the quotation:
+		print '<p>' . trim($data[$rand]) . '</p>';
 	?>
 </body>
 </html>
